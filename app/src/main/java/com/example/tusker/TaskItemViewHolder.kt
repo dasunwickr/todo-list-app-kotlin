d128bd4ef891b09ba6c2b5339b6870c4246d30e2
Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-05-18 12:14:42 +0530

    Error Fix

---
+++ app/src/main/java/com/example/tusker/TaskItemViewHolder.kt
@@ -0,0 +1,56 @@
+package com.example.tusker
+
+import android.content.Context
+import android.graphics.Paint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tusker.databinding.TaskItemCellBinding
+import java.time.format.DateTimeFormatter
+
+class TaskItemViewHolder(
+    private val context: Context,
+    private val binding: TaskItemCellBinding,
+    private val clickListener: TaskItemClickListener
+): RecyclerView.ViewHolder(binding.root){
+    @RequiresApi(Build.VERSION_CODES.O)
+    val timeFormat = DateTimeFormatter.ofPattern("HH:mm")!!
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun bindTaskItem(taskItem: TaskItem) {
+        binding.name.text = taskItem.name
+
+        if(taskItem.isCompleted()){
+            binding.name.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            binding.dueTime.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        }
+
+        binding.completeButton.setImageResource(taskItem.imageResource())
+        binding.completeButton.setColorFilter(taskItem.imageColor(context))
+
+        binding.completeButton.setOnClickListener {
+            clickListener.completeTaskItem(taskItem)
+        }
+
+        binding.deleteButton.setOnClickListener {
+            // Call the deleteTaskItem method in the clickListener interface
+            clickListener.deleteTaskItem(taskItem)
+        }
+
+        // Set background color based on priority
+        when (taskItem.priority) {
+            "Low" -> binding.root.setBackgroundColor(ContextCompat.getColor(context, R.color.low_priority_color))
+            "Medium" -> binding.root.setBackgroundColor(ContextCompat.getColor(context, R.color.medium_priority_color))
+            "High" -> binding.root.setBackgroundColor(ContextCompat.getColor(context, R.color.high_priority_color))
+        }
+
+        binding.taskCellContainer.setOnClickListener {
+            clickListener.editTaskItem(taskItem)
+        }
+
+        if (taskItem.dueTime() != null)
+            binding.dueTime.text = timeFormat.format(taskItem.dueTime())
+        else
+            binding.dueTime.text = ""
+    }
+}
