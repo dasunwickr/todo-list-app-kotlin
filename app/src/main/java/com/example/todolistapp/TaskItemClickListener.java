bede8bac92d4f94985227a5dc81abbdf849c2076
dasunwickr <sathsarawd@gmail.com>
2024-05-18 06:57:19 +0530

    Error fix

--- app/src/main/java/com/example/todolistapp/TaskItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.todolistapp;
-
-import com.example.todolistapp.TaskItem;
-
-interface TaskItemClickListener {
-    fun editTaskItem(taskItem:TaskItem)
-    fun completeTaskItem(taskItem: TaskItem)
-    fun deleteTaskItem(taskItem: TaskItem)
-}
